baa647d0e86f34fa964a534f0f2081ed3a274364
Nate Hollingshaus <hol19020@byui.edu>
2023-01-10 10:40:42 -0700

    adding css and js to homepage

---
+++ scripts/site.js
@@ -0,0 +1,7 @@
+// sete year for the footer
+const date = new Date()
+document.querySelector('#currentyear').textContent = date.getFullYear();
+
+//set last modififed in footer
+
+document.querySelector('#lastmodified').textContent = document.lastModified;
