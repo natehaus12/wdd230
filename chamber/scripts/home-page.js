cfaed8110e0e0181d57c1aa8fd2cb9cde9d48e9c
Nate Hollingshaus <hol19020@byui.edu>
2023-02-03 21:04:31 -0700

    mobile - chamber home

--- chamber/scripts/home-page.js
+++ chamber/scripts/home-page.js
@@ -8,3 +8,8 @@ function toggleMenu(){
 //document.querySelector("#hamburger-menu").onclick = toggleMenu;
 
 document.querySelector("#hamburger-menu").addEventListener('click', toggleMenu);
+
+var messagedate = new Date();
+if(messagedate.getDay() == 1 || messagedate.getDay() == 2){
+    document.querySelector("#meet-greet").classList.add('active')
+}
