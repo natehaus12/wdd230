076d1aabde3b25b309f1ee0f38f060c849f4b4c1
Nate Hollingshaus <hol19020@byui.edu>
2023-02-28 10:14:16 -0700

    json presentation and join finishes

---
+++ chamber/scripts/join.js
@@ -0,0 +1,9 @@
+let currentDate= new Date;
+let time = currentDate.getHours() + ":" + currentDate.getMinutes();
+
+let dateAndTime = currentDate + " " + time;
+
+var html = document.querySelector("#hiddenTime");
+html.value = dateAndTime;
+
+console.log(html.value)
